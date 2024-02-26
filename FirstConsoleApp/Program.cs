ba42a2f18fb73c457eec47fff75ffe43be1e2014
sujanamagar <magarsujana1@gmail.com>
2024-02-26 08:31:37 +0545

    changes

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+Console.WriteLine(3687426);
+
+DataTypes dt = new();
+dt.Learn();
+
+Methods m = new();
+m.Greet("Hi");
+m.CalculateBMI(233.3f, 343.34f);
+
+var highest = m.GetMax(3.4, 5.6, 78.4, 23.4);
+var highest2 = m.GetMax(3.4, 5.6);
+var highest3 = m.GetMax(3.4, 5.6, 78.4, 23.4, 67.8);
+var highest4 = m.GetMax(3.4);
--- README.md
+++
@@ -1,5 +0,0 @@
-![alt text](image.png)
-![alt text](image-1.png)
-![alt text](image-2.png)
-![alt text](image-4.png)
-![alt text](image-5.png)
